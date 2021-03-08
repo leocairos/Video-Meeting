412161e144a48e01235754b8bd32e5b45af0aa76
Leonardo <leocairos@gmail.com>
2021-03-08 12:46:47 -0300

    adjust

--- src/Video.js
+++ src/Video.js
@@ -20,7 +20,13 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
 import "./Video.css"
 
-const server_url = process.env.NODE_ENV === 'production' ? 'https://video.sebastienbiollo.com' : "http://localhost:4001"
+const server_url = 
+process.env.NODE_ENV === 'production' 
+//? 'https://video.sebastienbiollo.com' 
+? process.env.SERVER_URL_PROD
+//: "http://localhost:4001"
+: process.env.SERVER_URL_DEV
+
 
 var connections = {}
 const peerConnectionConfig = {
